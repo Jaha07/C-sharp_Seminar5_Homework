23fe28a7f50c8059667b40b18fd092ecdfc0ecae
Jaha <zlgsbazarbaev@gmail.com>
2023-02-09 16:32:52 +0500

    added task1

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 34: Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в
+// массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GetRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+int CountEvenInArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+int[] array = GetRandomArray(10, 100, 999);
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine($"The amount of even numbers: {CountEvenInArray(array)}");
